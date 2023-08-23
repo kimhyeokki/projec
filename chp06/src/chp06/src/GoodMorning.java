5074aaa5be9c765766d7c51d9842d9d935c123af
kimhyeokki <140989443+kimhyeokki@users.noreply.github.com>
2023-08-23 17:46:32 +0900

    Delete chp06/src/chp06/src directory

--- chp06/src/chp06/src/GoodMorning.java
+++
@@ -1,9 +0,0 @@
-import java.util.Calendar;
-
-public class GoodMorning {
-    public static void main(String[] args) {
-        Calendar calendar = Calendar.getInstance();
-        int hour =calendar.get(calendar.HOUR_OF_DAY);
-        
-    }
-}
